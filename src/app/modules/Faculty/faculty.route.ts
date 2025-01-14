2a88bbe15e4e32608da2205d0a1a918d5cc230c0
Md Yeasin <mdyeasinhn@gmail.com>
2025-01-14 15:16:35 +0600

    faculty route added

---
+++ src/app/modules/Faculty/faculty.route.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import validateRequest from '../../maddwares/validRequest';
+
+const router = express.Router();
+
+router.get('/:id', FacultyControllers.getSingleFaculty);
+
+router.patch(
+  '/:id',
+  validateRequest(updateFacultyValidationSchema),
+  FacultyControllers.updateFaculty,
+);
+
+router.delete('/:id', FacultyControllers.deleteFaculty);
+
+router.get('/', FacultyControllers.getAllFaculties);
+
+export const FacultyRoutes = router;
