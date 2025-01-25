488ef07a98674bf2635c56e55254fc4299be8410
mdyeasinhn <mdyeasinhn@gmail.com>
2025-01-25 23:27:47 +0600

    offered course service  added

--- src/app/modules/OfferedCourse/offeredCourse.controller.ts
+++ src/app/modules/OfferedCourse/offeredCourse.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import httpStatus from 'http-status';
 import catchAsync from '../../utils/catchAsync';
 import sendResponse from '../../utils/sendResponse';
+import { OfferedCourseServices } from './offeredCourse.service';
 
 
 const createOfferedCourse = catchAsync(async (req: Request, res: Response) => {
