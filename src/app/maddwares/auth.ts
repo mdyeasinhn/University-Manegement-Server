01fc586111832053a62b33e1772d0b6e9c88b12a
mdyeasinhn <mdyeasinhn@gmail.com>
2025-01-31 23:39:35 +0600

    Verify token using auth

--- src/app/maddwares/auth.ts
+++ src/app/maddwares/auth.ts
@@ -1,20 +1,30 @@
 import { NextFunction, Request, Response } from 'express';
-
 import catchAsync from '../utils/catchAsync';
 import AppError from '../errors/AppError';
 import httpStatus from 'http-status';
-
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import config from '../config';
 const auth = () => {
-  return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-    const token = req.headers.authorization;
+    return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+        const token = req.headers.authorization;
 
-       console.log(token);
-       if(!token){
-        throw new AppError(httpStatus.UNAUTHORIZED, "You are not authorized !")
-       }
-   next()
-  });
-  
+        console.log(token);
+        if (!token) {
+            throw new AppError(httpStatus.UNAUTHORIZED, "You are not authorized !")
+        }
+
+        // invalid token
+        jwt.verify(token, config.jwt_access_secret as string, function (err, decoded) {
+            // err
+            if (err) {
+                throw new AppError(httpStatus.UNAUTHORIZED, "You are not authorized !")
+            }
+            // decoded undefined
+            req.user = decoded as JwtPayload
+            next()
+        });
+    });
+
 };
 
 export default auth;
